ff58f102aa7ad45e6b09ddd29619cf4ce00dcb7a
Wesley Araujo <wesley.sickert@gmail.com>
2018-07-20 05:44:10 -0300

    add orders controller and pg to heroku in gemfile

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,18 @@
 class OrdersController < ApplicationController
+    before_filter :authorize
+
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    private
+    
+    def movie_params
+        params.require(:movie).permit(:name, :price, genre_attributes: [:id, :name])
+    end
 end
