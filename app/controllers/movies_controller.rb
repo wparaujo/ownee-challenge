8ccb9c1bc2ea33052f05c752f7d7bd94acfb1923
Wesley Araujo <wesley.sickert@gmail.com>
2018-07-20 06:16:48 -0300

    change mode of search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,7 +24,7 @@ class MoviesController < ApplicationController
     end
 
     def search
-        @movies = Movie.where('lower(name) = ?', params[:name].downcase)
+        @movies = Movie.where('lower(name) LIKE ?', "%#{params[:name].downcase}%")
         render action: :results
     end
 
